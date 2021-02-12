8f8f4c90c380f0767909d66ccb0fa2d79796c72d
serij-shmel <icloudit@gmail.com>
2021-02-12 17:05:50 +0700

    03_lesson1

--- src/Loader.java
+++ src/Loader.java
@@ -4,7 +4,31 @@ public class Loader
     public static void main(String[] args)
     {
         Cat cat = new Cat();
-
+        System.out.println(cat.getWeight());
         System.out.println(cat.getStatus());
+        //=================
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+
+        System.out.println(cat1.getWeight());
+        cat1.feed(2.0);
+        System.out.println(cat1.getWeight());
+        System.out.println(cat2.getWeight());
+        cat2.feed(3.0);
+        System.out.println(cat2.getWeight());
+        //=================
+        Cat cat3 = new Cat();
+        System.out.println("вес 3 кошки= "+cat3.getWeight());
+        cat3.feed(10000.0);
+        System.out.println("вес 3 кошки= "+cat3.getWeight());
+        System.out.println("статус 3 кошки= "+cat3.getStatus());
+        //=================
+        int i=1;
+        while (!(cat2.getStatus()=="Dead")) {
+            cat2.meow();
+        }
+        System.out.println("статус 2 кошки= "+cat2.getStatus());
+
+
     }
 }
