9150cd209422fb29b38b7de553ea63cb2652a196
serij-shmel <icloudit@gmail.com>
2021-02-13 23:42:14 +0700

    03_lesson6

--- RoadController/src/core/Car.java
+++ RoadController/src/core/Car.java
@@ -13,4 +13,54 @@ public class Car {
             special + "Автомобиль с номером " + number +
             ":\n\tВысота: " + height + " мм\n\tМасса: " + weight + " кг";
     }
+
+    public void setnumber(String setnumber)
+    {
+        number = setnumber;
+    }
+
+    public String getnumber()
+    {
+        return number;
+    }
+
+    public void setheight(int setheight)
+    {
+        height = setheight;
+    }
+
+    public int getheight()
+    {
+        return height;
+    }
+
+    public void setweight(double setweight)
+    {
+        weight = setweight;
+    }
+
+    public double getweight()
+    {
+        return weight;
+    }
+
+    public void  sethasVehicle(boolean sethasVehicle)
+    {
+        hasVehicle = sethasVehicle;
+    }
+
+    public boolean gethasVehicle()
+    {
+        return hasVehicle;
+    }
+
+    public void  setisSpecial(boolean setisSpecial)
+    {
+        isSpecial = setisSpecial;
+    }
+
+    public boolean getisSpecial()
+    {
+        return isSpecial;
+    }
 }
